3b0aa18cc3e1dcea645f10e79922c91f1fcc9303
danlester <danlester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-06 19:45:13 +0000

    version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/google-apps-login/tags/1.4@834067 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google_apps_login.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Plugin Name: Google Apps Login
+ * Plugin URI: http://wp-glogin.com/
+ * Description: Simple secure login for Wordpress through users' Google Apps accounts (uses secure OAuth2, and MFA if enabled)
+ * Version: 1.4
+ * Author: Dan Lester
+ * Author URI: http://wp-glogin.com/
+ * License: GPL3
+ * Network: true
+ */
+
+require_once( plugin_dir_path(__FILE__).'/core/core_google_apps_login.php' );
+
+class basic_google_apps_login extends core_google_apps_login {
+	
+	protected $PLUGIN_VERSION = '1.4';
+	
+	public function ga_section_text_end() {
+	?>
+		<p><b>For full support, and premium features that greatly simplify WordPress user management for admins, please visit:
+		<a href="http://wp-glogin.com/?utm_source=Admin%20Promo&utm_medium=freemium&utm_campaign=Freemium" target="_blank">http://wp-glogin.com/</a></b>
+		</p>
+	<?php
+	}
+	
+	public function my_plugin_basename() {
+		$basename = plugin_basename(__FILE__);
+		if ('/'.$basename == __FILE__) { // Maybe due to symlink
+			$basename = basename(dirname(__FILE__)).'/'.basename(__FILE__);
+		}
+		return $basename;
+	}
+
+}
+
+$ga_google_apps_login_plugin = new basic_google_apps_login();
+
+?>
