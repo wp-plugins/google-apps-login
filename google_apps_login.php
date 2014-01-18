a8119d1e3bacb0e6a61e4165101e1e264f013820
danlester <danlester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-18 16:18:23 +0000

    Version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/google-apps-login/tags/2.0@840983 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google_apps_login.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Plugin Name: Google Apps Login
+ * Plugin URI: http://wp-glogin.com/
+ * Description: Simple secure login for Wordpress through users' Google Apps accounts (uses secure OAuth2, and MFA if enabled)
+ * Version: 2.0
+ * Author: Dan Lester
+ * Author URI: http://wp-glogin.com/
+ * License: GPL3
+ * Network: true
+ */
+
+require_once( plugin_dir_path(__FILE__).'/core/core_google_apps_login.php' );
+
+class basic_google_apps_login extends core_google_apps_login {
+	
+	protected $PLUGIN_VERSION = '2.0';
+	
+	// Singleton
+	private static $instance = null;
+	
+	public static function get_instance() {
+		if (null == self::$instance) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+		
+	public function ga_section_text_end() {
+	?>
+		<p><b>For full support, and premium features that greatly simplify WordPress user management for admins, please visit:
+		<a href="http://wp-glogin.com/?utm_source=Admin%20Promo&utm_medium=freemium&utm_campaign=Freemium" target="_blank">http://wp-glogin.com/</a></b>
+		</p>
+	<?php
+	}
+	
+	public function my_plugin_basename() {
+		$basename = plugin_basename(__FILE__);
+		if ('/'.$basename == __FILE__) { // Maybe due to symlink
+			$basename = basename(dirname(__FILE__)).'/'.basename(__FILE__);
+		}
+		return $basename;
+	}
+
+}
+
+// Global accessor function to singleton
+function GoogleAppsLogin() {
+	return basic_google_apps_login::get_instance();
+}
+
+// Initialise at least once
+GoogleAppsLogin();
+
+?>
